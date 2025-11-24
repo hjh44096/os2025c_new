ce88cd7864d5455fedf51082238ccd8d33cbb5b3
hjh44096 <hanjuheon126@gmail.com>
2025-11-24 18:34:41 +0900

    week13 enbeddingv0.1

--- week13/cmd/ch10~ex01/main.go
+++ week13/cmd/ch10~ex01/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"week13/pkg/calendar"
 )
 
 func main() {
-	today := calendar.Date{}
-	//today.Year = 2025
-	today.SetYear(2025)
-	today.SetMonth(11)
-	today.SetDay(24)
-	fmt.Println(today.Year(), "년", today.Month(), "월", today.Day(), "일")
+	today := calendar.Event{} //Date가 임베딩되어 있음
+	err := today.SetTitle("Final Exem D-14")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = today.SetYear(2025)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = today.SetMonth(11)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = today.SetDay(24)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Println(today.Year(), "년", today.Month(), "월", today.Day(), "일")
+	fmt.Printf("%s\n%d년 %d월 %d일\n", today.Title(), today.Year(), today.Month(), today.Day())
 }
